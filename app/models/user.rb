54297281c38f2bbe017b9d11fd141f522ca0b15d
Simone Biondo <879899@stud.unive.it>
2025-12-21 14:33:04 +0100

    init: run devise installers

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+
+  include DeviseTokenAuth::Concerns::User
+end
