ec2d69237c59dbd5c8a67cf8f9c02941681be0c7
Simone Biondo <879899@stud.unive.it>
2025-12-22 21:10:16 +0100

    init: add base entrypoint.sh for starting rails app

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This command will create the database, load the schema, and initialize it with the seed data - if any.
+# This task operates idempotently, so it can safely be called several times, but it will only perform the necessary
+# tasks once.
+bundle exec rails db:prepare
+
+# Start the Rails server, binding it to all network interfaces (0.0.0.0).
+# It is then accessible from outside the container on the specified port.
+bundle exec rails s -b 0.0.0.0 -p "$RAILS_PORT"
