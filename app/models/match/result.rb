2ac941cfed70ac51e216f2796bee7eca7788b73b
Simone Biondo <879899@stud.unive.it>
2025-12-24 20:09:12 +0100

    init: update with winners

--- app/models/match/result.rb
+++ app/models/match/result.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: match_results
+#
+#  id          :integer          not null, primary key
+#  match_id    :integer
+#  user_id     :integer
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_match_results_on_match_id  (match_id)
+#  index_match_results_on_user_id   (user_id)
+#
 class Match
-  # == Schema Information
-  #
-  # Table name: match_results
-  #
-  #  id          :integer          not null, primary key
-  #  match_id    :integer
-  #  user_id     :integer
-  #  description :string
-  #  created_at  :datetime         not null
-  #  updated_at  :datetime         not null
-  #
-  # Indexes
-  #
-  #  index_match_results_on_match_id  (match_id)
-  #  index_match_results_on_user_id   (user_id)
-  #
   class Result < ApplicationRecord
     belongs_to :match
 
@@ -24,5 +24,8 @@ class Match
                class_name:  'User::Referee',
                foreign_key: 'user_id',
                inverse_of:  :match_results
+
+    has_many :match_result_winners, class_name: 'Match::Result::Winner', inverse_of: :match_result
+    has_many :winners, through: :match_result_winners, source: :player
   end
 end
