a1484052583dd984011dd32cb4d27a6a53002439
Simone Biondo <879899@stud.unive.it>
2025-12-24 19:02:11 +0100

    init: add user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: users
@@ -34,10 +36,78 @@
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
 
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Helpers::Model
+
+  def types
+    User
+      .constants
+      .map { "User::#{it}" }
+      .map(&:constantize)
+      .filter { it.is_a? Class }
+      .filter { it < User }
+  end
+
+  test 'user creation' do
+    assert_nothing_raised do
+      types.each { |type| create(:user, :with_email, type: type.to_s) }
+    end
+  end
+
+  test 'user type' do
+    types.each do |type|
+      create(:user, :with_email, type: type.to_s)
+        .then { assert_instance_of type, type.public_send(:last) }
+    end
+  end
+
+  test 'user with attendances' do
+    create_match
+      .then do |match|
+        viewers = types
+                  .map do |type|
+                    create(:user, :with_email, type: type.to_s)
+                      .tap { create :match_viewer, match: match, viewer: it }
+                  end
+
+        [match, viewers]
+      end.then do |match, viewers|
+        viewers.each { assert_includes match.viewers, it }
+        viewers.each { assert_includes it.attendances, match }
+      end
+  end
+
+  test 'player with matches' do
+    create_match
+      .tap { |match| create :match_player, match: match, player: create_player }
+      .then do |match|
+        assert_includes User::Player.last!.matches, match
+        assert_includes match.players, User::Player.last!
+      end
+  end
+
+  test 'player with wins' do
+    [create_player, create(:match_result, match: create_match, referee: create_referee)].then do |player, result|
+      create(:match_result_winner, match_result: result, player: player)
+        .then do
+          # Player should see the result in won_matches
+          assert_includes player.won_matches, result
+
+          # The result should list the player as a winner
+          assert_includes result.winners, player
+        end
+    end
+  end
+
+  test 'referee with matches' do
+    [create_match, create_referee].then do |match, referee|
+      create(:match_result, match: match, referee: referee)
+        .then do |result|
+          assert_includes referee.match_results, result
+          assert_equal result.referee, referee
+        end
+    end
+  end
 end
