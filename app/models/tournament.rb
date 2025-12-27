10673b52a2230644ba9706f656b0f00a4ddee157
Simone Biondo <879899@stud.unive.it>
2025-12-27 21:11:19 +0100

    init: update tournament with associations

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,27 +1,32 @@
-# frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: tournaments
 #
-#  id            :integer          not null, primary key
-#  user_id       :integer
-#  sport         :integer          default(0)
-#  gender        :integer          default(0), not null
-#  min_birthdate :datetime
-#  max_birthdate :datetime
-#  start_date    :datetime
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
+#  id           :integer          not null, primary key
+#  name         :string
+#  description  :string
+#  start_date   :datetime
+#  end_date     :datetime
+#  organizer_id :integer
+#  sport_id     :integer
+#  field_id     :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 # Indexes
 #
-#  index_tournaments_on_user_id  (user_id)
+#  index_tournaments_on_field_id      (field_id)
+#  index_tournaments_on_organizer_id  (organizer_id)
+#  index_tournaments_on_sport_id      (sport_id)
 #
+
+# frozen_string_literal: true
+
 class Tournament < ApplicationRecord
-  include WithGender
+  belongs_to :field
+  belongs_to :sport
+  belongs_to :organizer, class_name: 'User::Organizer'
 
-  has_many :matches, dependent: :destroy
-
-  belongs_to :organizer, class_name: 'User::Organizer', foreign_key: 'user_id', inverse_of: :tournaments
+  has_many :players_tournaments, dependent: :destroy
+  has_many :players, through: :players_tournaments
 end
