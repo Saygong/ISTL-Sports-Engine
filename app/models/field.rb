2a9a81fd89bf6a07d176ce7cdb13fb769973931d
Simone Biondo <879899@stud.unive.it>
2025-12-27 21:08:14 +0100

    init: add field and sport

---
+++ app/models/field.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: fields
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  surface     :integer          default(0), not null
+#  max_seats   :integer
+#  courts      :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Field < ApplicationRecord
+  enum :surface, { grass: 0, hard: 1 }
+end
