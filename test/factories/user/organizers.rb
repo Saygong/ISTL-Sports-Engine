5e7268dade23ef4f27a2c515154c75834ba3c502
Simone Biondo <879899@stud.unive.it>
2025-12-28 10:36:31 +0100

    init: add users factories

---
+++ test/factories/user/organizers.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :user_organizer, class: 'User::Organizer', parent: :user do
+    # Nothing to do
+  end
+end
---
+++ test/factories/user/referees.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :user_referee, class: 'User::Referee', parent: :user do
+    # Nothing to do
+  end
+end
