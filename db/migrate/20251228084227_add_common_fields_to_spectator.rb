d150ec0b42136bf1f5fae5c4b8205d3b38dfca9f
Simone Biondo <879899@stud.unive.it>
2025-12-28 15:05:35 +0100

    init: remove models

--- db/migrate/20251228084227_add_common_fields_to_spectator.rb
+++
@@ -1,8 +0,0 @@
-class AddCommonFieldsToSpectator < ActiveRecord::Migration[8.1]
-  def change
-    add_column :spectators, :first_name, :string
-    add_column :spectators, :last_name, :string
-    add_column :spectators, :birthdate, :datetime
-    add_column :spectators, :gender, :integer, default: 0, null: false
-  end
-end
