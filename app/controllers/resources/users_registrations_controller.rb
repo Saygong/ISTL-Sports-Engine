2dfbd134270dc4f08132c2e7b847dd45026d2268
Simone Biondo <879899@stud.unive.it>
2025-12-29 17:47:43 +0100

    devise: by default only player can register autonomously

--- app/controllers/resources/users_registrations_controller.rb
+++ app/controllers/resources/users_registrations_controller.rb
@@ -2,6 +2,13 @@
 
 module Resources
   class UsersRegistrationsController < Devise::RegistrationsController
+    def build_resource hash = {}
+      # This forces the resulting object to be an instance of User::Player, since it is the only kind of us er that can
+      # register autonomously.
+      hash[:type] = 'User::Player'
+      super
+    end
+
     # If the account that is registered is confirmable and not active yet, you have to override
     # +after_inactive_sign_up_path_for+ method.
     # [https://github.com/heartcombo/devise/wiki/How-Tos]
