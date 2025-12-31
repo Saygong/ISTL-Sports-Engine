cbb047133efe19fde8ab1be71e820881f35070db
Simone Biondo <879899@stud.unive.it>
2025-12-31 16:09:30 +0100

    models: create courts and fields

---
+++ app/models/court.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: courts
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Court < ApplicationRecord
+  has_many :fields, class_name: 'Court::Field', dependent: :destroy
+end
