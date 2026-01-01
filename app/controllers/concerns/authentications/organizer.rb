110d0ddc952b130b158e453ded9a895f835ce235
Simone Biondo <879899@stud.unive.it>
2026-01-01 14:38:05 +0100

    controllers: rename method

--- app/controllers/concerns/authentications/organizer.rb
+++ app/controllers/concerns/authentications/organizer.rb
@@ -5,7 +5,7 @@ module Authentications
     extend ActiveSupport::Concern
 
     class_methods do
-      def ensure_organizer!(*args, **kwargs)
+      def require_organizer!(*args, **kwargs)
         before_action(*args, **kwargs) do
           unless current_user.is_a?(User::Organizer)
             redirect_to root_path, alert: I18n.t('errors.controllers.organizers.unauthorized')
