43c136ee98cbeb7ba067af029fd13d87bdba4586
Simone Biondo <879899@stud.unive.it>
2026-01-01 19:45:32 +0100

    controllers: update referees_controller

--- app/controllers/referees_controller.rb
+++ app/controllers/referees_controller.rb
@@ -1,20 +1,19 @@
 class RefereesController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_referee!
 
-  #GET - Returns all tournament with matches that the user is referee of
-  def show
-    #@tournaments = Tournament.includes(:matches).with_match_refereed_by(current_user)
-    @results_by_match_id = Match::Result.where(match_id: @tournaments.flat_map(&:matches).map(&:id)).index_by(&:match_id)
-  end
-
-
-
-  private
-  def require_referee!
+  before_action only: [:show] do
     unless current_user.is_a?(User::Referee)
-      redirect_to root_path, alert: "You must be a referee to access the referee homepage."
+      redirect_to root_path, alert: I18n.t('errors.controllers.referees.unauthorized')
     end
   end
 
+  # GET - Returns all tournament with matches that the user is referee of
+  def show
+    # @tournaments = Tournament.includes(:matches).with_match_refereed_by(current_user)
+    @results_by_match_id = Match::Result.where(match_id: @tournaments.flat_map(&:matches).map(&:id)).index_by(&:match_id)
+  end
+
+  def profile
+    redirect_to profile_user_path
+  end
 end
