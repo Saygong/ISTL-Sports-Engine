6f0fd7c263b7b78274f9f0f49d78ee01cc122b31
Simone Biondo <879899@stud.unive.it>
2026-01-03 15:16:20 +0100

    controllers: add rescue from to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  # ...
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    redirect_back fallback_location: root_path,
+                  alert:             exception.record.errors.full_messages.to_sentence
+  end
 end
