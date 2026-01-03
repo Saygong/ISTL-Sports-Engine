f544424fa760645246b7bb71097d719d8b9ee629
AndreaGentilini <andreagentilini5@gmail.com>
2026-01-03 15:47:51 +0100

    Configured ngrok endpoint

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  #Allow Ngrok host
+  config.hosts << "kurtis-unstaggered-stanley.ngrok-free.dev"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
