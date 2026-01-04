d0124727a8fffb7bd3fe0057202d58486c3d0541
Simone Biondo <879899@stud.unive.it>
2026-01-04 14:05:01 +0100

    models: add status to tournament + improvements

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,21 +5,22 @@
 # Table name: tournaments
 #
 #  id                :integer          not null, primary key
+#  composition       :integer          default(0), not null
+#  court_id          :integer
 #  created_at        :datetime         not null
 #  description       :string
 #  end_date          :datetime
 #  field_id          :integer
+#  gender            :integer          default(0), not null
+#  max_age           :integer
+#  min_age           :integer
 #  name              :string
+#  number_of_matches :integer
 #  organizer_id      :integer
 #  sport_id          :integer
 #  start_date        :datetime
 #  updated_at        :datetime         not null
-#  max_age           :integer
-#  min_age           :integer
-#  gender            :integer          default(0), not null
-#  number_of_matches :integer
-#  court_id          :integer
-#  composition       :integer          default(0), not null
+#  status            :integer          default(0)
 #
 # Indexes
 #
@@ -57,6 +58,13 @@ class Tournament < ApplicationRecord
     errors.add(:base, I18n.t('errors.models.tournament.min_age_gt_max_age')) if min_age > max_age
   end
 
+  # ...
+  enum :status, {
+    waiting:   0,
+    started:   1,
+    completed: 2
+  }
+
   # Scope that accepts a hash of search parameters, name, sport, and date.
   scope :filter_by, lambda { |**cols|
     day = Date.parse(cols[:start_date]) rescue nil # rubocop:disable Style/RescueModifier
@@ -71,8 +79,8 @@ class Tournament < ApplicationRecord
   # All tournaments where a specific player's profile meets the requirements.
   scope :joinable_by, lambda { |player|
     # noinspection SqlNoDataSourceInspection
-    where('min_age >= ?', player.years_from_birth)
-      .where('max_age <= ?', player.years_from_birth)
+    where('min_age <= ?', player.years_from_birth)
+      .where('max_age >= ?', player.years_from_birth)
       .where(gender: player.gender)
   }
 
@@ -89,4 +97,53 @@ class Tournament < ApplicationRecord
     number_of_matches * 2
   end
 
+  # probabile validation per expired -> il tournament non ha seeded match e start date < now
+
+  # ...
+  # Questo andrà in un change di stato.
+  def match_seeding!
+    # ... ready?
+    return nil unless teams.count == teams_capacity
+    return nil unless waiting?
+
+    # ...
+    matches_per_day = court
+                      .fields
+                      .count
+
+    transaction do
+      # noinspection RubyNilAnalysis
+      number_of_matches
+        .times do |index|
+          # ...
+          selected_teams = teams
+                           .offset(index * 2) # It's like between?(index * 2, (index * 2) + 1)
+                           .limit(2)
+
+          (start_date + (index / matches_per_day).days)
+            .advance(12.hours)
+            .then do |date|
+              matches.create! teams: selected_teams,
+                              field: court.fields[index % matches_per_day],
+                              date:  date,
+                              round: 0
+            end
+        end
+
+      # ...
+      started!
+    end
+  end
+
+  # ...
+  # def create_match! player
+  #   ActiveRecord::Base.transaction do
+  #     matches
+  #       .create!
+  #       .teams
+  #       .create!(composition: composition)
+  #       .players_teams
+  #       .create! player: player
+  #   end
+  # end
 end
