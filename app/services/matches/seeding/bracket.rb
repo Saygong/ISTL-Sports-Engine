7583f2b5b14cc11dd0b63113add588c2a19cdd90
Simone Biondo <879899@stud.unive.it>
2026-01-05 14:32:59 +0100

    services: add bracket for matches

---
+++ app/services/matches/seeding/bracket.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Matches
+  module Seeding
+    class Bracket < ::Seeding::Base
+      def seed? match
+        # A match is eligible if it is currently "incomplete" (missing teams/players)
+        return false unless Match.incomplete.find(match.id)
+
+        true
+      end
+
+      def start_seed match
+        # Populate the game with teams by identifying the winners of the prerequisite matches
+        match
+          .matches
+          .map(&:match_result)
+          .compact
+          .map(&:winner)
+          .then { |teams| match.update! teams: teams }
+      end
+    end
+  end
+end
