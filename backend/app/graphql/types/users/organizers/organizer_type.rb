d7e285c1ec1cfa894e6d5a564f05eaca15c1a26c
Simone Biondo <879899@stud.unive.it>
2026-01-11 11:06:51 +0100

    add organizer_type

---
+++ backend/app/graphql/types/users/organizers/organizer_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  module Users
+    module Organizers
+      class OrganizerType < Types::BaseObject
+        implements UserInterfaceType
+      end
+    end
+  end
+end
