08b373618f4c834337f46e35eec34a25a0d3dba4
Simone Biondo <879899@stud.unive.it>
2026-01-11 22:09:45 +0100

    add queries (player)

--- backend/app/graphql/types/users/players/player_type.rb
+++ backend/app/graphql/types/users/players/player_type.rb
@@ -10,6 +10,26 @@ module Types
               [Tournaments::TournamentType],
               null: false
 
+        field :matches_to_play,
+              [Matches::MatchType],
+              null: false
+
+        field :viewed_matches,
+              [Matches::MatchType],
+              null: false
+
+        def matches_to_play
+          # TODO: Only player should see the matches to play
+          context[:current_user]
+            .then do |player|
+              Match
+                .joins(teams: :players_teams)
+                .where(players_teams: { player_id: player.id })
+                .order(date:  :asc, round: :asc)
+                .distinct
+            end
+        end
+
         def joinable_tournaments
           # TODO: Only player should see the joinable tournaments
           context[:current_user]
