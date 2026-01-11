a84e1d989477364a7c310e6fbe86078a6d5f0fb5
Simone Biondo <879899@stud.unive.it>
2026-01-11 21:50:13 +0100

    add joinable tournaments to player

--- backend/app/graphql/types/users/players/player_type.rb
+++ backend/app/graphql/types/users/players/player_type.rb
@@ -5,6 +5,16 @@ module Types
     module Players
       class PlayerType < Types::BaseObject
         implements UserInterfaceType
+
+        field :joinable_tournaments,
+              [Tournaments::TournamentType],
+              null: false
+
+        def joinable_tournaments
+          # TODO: Only player should see the joinable tournaments
+          context[:current_user]
+            .then { |player| Tournament.joinable_by(player) }
+        end
       end
     end
   end
