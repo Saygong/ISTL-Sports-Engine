56531cc58eef2f59aca29172bf2be840a5057314
Simone Biondo <879899@stud.unive.it>
2026-01-11 10:22:39 +0100

    add enum types to graphql

---
+++ backend/app/graphql/types/composition_enum_type.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Types
+  class CompositionEnumType < Types::BaseEnum
+    value :single
+    value :double
+  end
+end
---
+++ backend/app/graphql/types/gender_enum_type.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Types
+  class GenderEnumType < Types::BaseEnum
+    value :male
+    value :female
+  end
+end
