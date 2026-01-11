6f8c15ae66e3ceaaed9dc9c8a70371d5eef24b29
Simone Biondo <879899@stud.unive.it>
2026-01-11 21:45:27 +0100

    add id to sport_type

--- backend/app/graphql/types/sports/sport_type.rb
+++ backend/app/graphql/types/sports/sport_type.rb
@@ -3,6 +3,7 @@
 module Types
   module Sports
     class SportType < Types::BaseObject
+      field :id, ID, null: false
       field :variant_kind, VariantKindEnumType, null: false
       field :description, String, null: true
     end
