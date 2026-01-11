a61b6cac14b1362e5102b54d0ee322eefd65016d
Simone Biondo <879899@stud.unive.it>
2026-01-11 10:55:26 +0100

    add user_interface_type

---
+++ backend/app/graphql/types/users/user_interface_type.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  module Users
+    module UserInterfaceType
+      include Types::BaseInterface
+
+      field :email, String, null: false
+      field :data, GraphQL::Types::JSON, null: false
+      field :first_name, String, null: false
+      field :last_name, GraphQL::Types::JSON, null: false
+      field :birthdate, GraphQL::Types::ISO8601Date, null: false
+      field :gender, GenderEnumType, null: false
+    end
+  end
+end
