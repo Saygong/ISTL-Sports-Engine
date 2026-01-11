f41f997ce170e56a809155a216ca901fc0442ed6
Simone Biondo <879899@stud.unive.it>
2026-01-11 21:13:23 +0100

    add player_type

---
+++ backend/app/graphql/types/users/players/player_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  module Users
+    module Players
+      class PlayerType < Types::BaseObject
+        implements UserInterfaceType
+      end
+    end
+  end
+end
