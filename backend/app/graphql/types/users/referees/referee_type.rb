1b564ccd32fde2d3219b67fa7236034e8b2bdba7
Simone Biondo <879899@stud.unive.it>
2026-01-12 21:54:53 +0100

    update referee_type with refereed_tournaments

--- backend/app/graphql/types/users/referees/referee_type.rb
+++ backend/app/graphql/types/users/referees/referee_type.rb
@@ -5,6 +5,21 @@ module Types
     module Referees
       class RefereeType < Types::BaseObject
         implements UserInterfaceType
+
+        field :refereed_tournaments,
+              [Tournaments::TournamentType],
+              null: false
+
+        def refereed_tournaments
+          context[:current_user]
+            .then do |referee|
+              Tournament
+                .joins(:matches)
+                .where(matches: { referee_id: referee })
+                .order(start_date: :asc)
+                .distinct
+            end
+        end
       end
     end
   end
