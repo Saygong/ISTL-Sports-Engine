ebd28af02ab20a39af1d96f613c0506c24c32529
Simone Biondo <879899@stud.unive.it>
2026-01-12 21:59:42 +0100

    add result_type

---
+++ backend/app/graphql/types/matches/results/result_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  module Matches
+    module Results
+      class ResultType < Types::BaseObject
+        field :id, ID, null: false
+      end
+    end
+  end
+end
