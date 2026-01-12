f66577489d4c5c78e0e2245d2a6176b3857e2b2e
Simone Biondo <879899@stud.unive.it>
2026-01-12 21:35:50 +0100

    add tournament query by id

--- backend/app/graphql/types/tournaments/queries.rb
+++ backend/app/graphql/types/tournaments/queries.rb
@@ -8,6 +8,10 @@ module Types
           field :tournaments_all, [TournamentType], null: false do
             argument :search, TournamentSearchInput, required: false
           end
+
+          field :tournament, TournamentType, null: false do
+            argument :id, GraphQL::Types::ID, required: true
+          end
         end
       end
 
@@ -17,6 +21,10 @@ module Types
           .then { search&.[](:name) ? it.where(name: search[:name]) : it }
           .then { search&.[](:sport_id) ? it.where(name: search[:sport_id]) : it }
           .then { search&.[](:start_date) ? it.where(start_date: search[:start_date]) : it }
+      end
+
+      def tournament id:
+        Tournament.find id
       end
     end
   end
