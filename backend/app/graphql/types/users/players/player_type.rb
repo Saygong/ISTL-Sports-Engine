7a118813647542311dca292a5295a0e384b6d838
Simone Biondo <879899@stud.unive.it>
2026-01-12 21:36:58 +0100

    update player_type

--- backend/app/graphql/types/users/players/player_type.rb
+++ backend/app/graphql/types/users/players/player_type.rb
@@ -6,32 +6,33 @@ module Types
       class PlayerType < Types::BaseObject
         implements UserInterfaceType
 
+        # TODO: Only player should see this field
         field :joinable_tournaments,
               [Tournaments::TournamentType],
               null: false
 
+        # TODO: Only player should see this field
         field :matches_to_play,
               [Matches::MatchType],
               null: false
 
+        # TODO: Only player should see this field
         field :viewed_matches,
               [Matches::MatchType],
               null: false
 
         def matches_to_play
-          # TODO: Only player should see the matches to play
           context[:current_user]
             .then do |player|
               Match
                 .joins(teams: :players_teams)
                 .where(players_teams: { player_id: player.id })
-                .order(date:  :asc, round: :asc)
+                .order(date: :asc, round: :asc)
                 .distinct
             end
         end
 
         def joinable_tournaments
-          # TODO: Only player should see the joinable tournaments
           context[:current_user]
             .then { |player| Tournament.joinable_by(player) }
         end
