94f57c1b09b03ab13e74042a52ce5b6867d00f28
Simone Biondo <879899@stud.unive.it>
2026-01-17 22:15:32 +0100

    fix sign-up

--- backend/app/graphql/mutations/users/sign_up.rb
+++ backend/app/graphql/mutations/users/sign_up.rb
@@ -3,10 +3,21 @@
 module Mutations
   module Users
     class SignUp < BaseMutation
-      class SignUpArgs < Types::BaseInputObject
+      class AuthDataArgs < Types::BaseInputObject
         argument :email, String, required: true
         argument :password, String, required: true
         argument :password_confirmation, String, required: true
+      end
+
+      class UserDataArgs < Types::BaseInputObject
+        argument :first_name, String, required: true
+        argument :last_name, String, required: true
+        argument :birthdate, GraphQL::Types::ISO8601Date, required: true
+      end
+
+      class SignUpArgs < Types::BaseInputObject
+        argument :auth, AuthDataArgs, required: true
+        argument :user, UserDataArgs, required: true
       end
 
       argument :args, SignUpArgs, required: true
@@ -14,9 +25,12 @@ module Mutations
 
       def resolve args:
         user = User::Player
-               .create! email:                 args[:email],
-                        password:              args[:password],
-                        password_confirmation: args[:password_confirmation]
+               .create! email:                 args[:auth][:email],
+                        password:              args[:auth][:password],
+                        password_confirmation: args[:auth][:password_confirmation],
+                        first_name:            args[:user][:first_name],
+                        last_name:             args[:user][:first_name],
+                        birthdate:             args[:user][:birthdate]
 
         Types::Users::SignActionType
           .to_h user, user.create_new_auth_token
