7e270302340e4dc277e8779461a1051bc652319b
Simone Biondo <879899@stud.unive.it>
2026-01-17 18:13:34 +0100

    update with roles

--- backend/app/graphql/types/base_authenticated_field.rb
+++ backend/app/graphql/types/base_authenticated_field.rb
@@ -2,12 +2,25 @@
 
 module Types
   class BaseAuthenticatedField < BaseField
-    def initialize(*args, **kwargs, &)
+    def initialize(*args, role: :base, **kwargs, &)
+      @role = role
+
       super
     end
 
     def authorized? obj, args, ctx
-      return true if super && ctx[:current_user].present?
+      auth_procs = {
+        base:      proc { ctx[:current_user].present? },
+        player:    proc { ctx[:current_user].is_a? User::Player },
+        organizer: proc { ctx[:current_user].is_a? User::Organizer },
+        referee:   proc { ctx[:current_user].is_a? User::Referee }
+      }
+
+      if @role.present?
+        return true if super && auth_procs[@role]&.call
+      elsif super
+        return true
+      end
 
       raise ErrorsGQL::Unauthorized, I18n.t('errors.query.unauthorized')
     end
