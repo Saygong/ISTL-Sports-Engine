2f443022b3237b5044a9789be8088c2ae922043c
Simone Biondo <879899@stud.unive.it>
2026-01-18 14:26:15 +0100

    add organizer tournaments

--- backend/app/graphql/types/users/organizers/organizer_type.rb
+++ backend/app/graphql/types/users/organizers/organizer_type.rb
@@ -5,6 +5,19 @@ module Types
     module Organizers
       class OrganizerType < Types::BaseObject
         implements UserInterfaceType
+
+        # noinspection RubyArgCount
+        field :tournaments,
+              [Tournaments::TournamentType],
+              null: false,
+              role: :organizer
+
+        def tournaments
+          context[:current_user]
+            .then do |organizer|
+              Tournament.where(organizer: organizer)
+            end
+        end
       end
     end
   end
