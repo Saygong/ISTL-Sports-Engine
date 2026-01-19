359265c120889e57e5d7ad62529550e8cb391187
Simone Biondo <879899@stud.unive.it>
2026-01-19 20:48:54 +0100

    FIX Cors: attempt 1

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -40,5 +40,18 @@ module IstlSportsEngine
 
     # Set default locale
     config.i18n.default_locale = :en
+
+    config.middleware.insert_before Warden::Manager, Rack::Cors do
+      allow do
+        origins %r{http://localhost(:\d+)?},
+                %r{http://127\.0\.0\.1(:\d+)?}
+
+        # resource '*',
+        #          methods: [:get, :post, :put, :patch, :delete, :options],
+        #          headers: :any,
+        #          expose:  %w[access-token expiry token-type uid client],
+        #          max_age: 600
+      end
+    end
   end
 end
