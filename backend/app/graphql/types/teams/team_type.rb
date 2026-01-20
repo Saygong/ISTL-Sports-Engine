8c02d014c9df4cd6cfd0b975ba57640055740e24
Simone Biondo <879899@stud.unive.it>
2026-01-20 21:10:37 +0100

    add fields to team type

--- backend/app/graphql/types/teams/team_type.rb
+++ backend/app/graphql/types/teams/team_type.rb
@@ -4,7 +4,9 @@ module Types
   module Teams
     class TeamType < Types::BaseObject
       field :id, GraphQL::Types::ID, null: false
+      field :name, GraphQL::Types::String, null: true
       field :composition, Common::CompositionEnumType, null: false
+      field :players, [Users::Players::PlayerType], null: false
     end
   end
 end
