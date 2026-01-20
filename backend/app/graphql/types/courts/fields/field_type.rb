61d1ccff077981bade987726183e52c6c8a196c2
Simone Biondo <879899@stud.unive.it>
2026-01-20 21:14:50 +0100

    add fields to field type

--- backend/app/graphql/types/courts/fields/field_type.rb
+++ backend/app/graphql/types/courts/fields/field_type.rb
@@ -5,6 +5,11 @@ module Types
     module Fields
       class FieldType < Types::BaseObject
         field :id, GraphQL::Types::ID, null: false
+        field :name, GraphQL::Types::String, null: true
+        field :description, GraphQL::Types::String, null: true
+        field :max_seats, GraphQL::Types::Int, null: true
+        field :court, Types::Courts::CourtType, null: false
+        field :matches, [Matches::MatchType], null: false
       end
     end
   end
