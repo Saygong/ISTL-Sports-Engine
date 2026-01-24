2b69e341fb2d126e4dd49564bf731c3593761309
Simone Biondo <879899@stud.unive.it>
2026-01-24 11:25:37 +0100

    minor

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,7 @@
 # tasks once.
 bundle exec rails db:prepare
 bundle exec rails db:migrate
+
 # Start the Rails server, binding it to all network interfaces (0.0.0.0).
 # It is then accessible from outside the container on the specified port.
 bundle exec rails s -b 0.0.0.0 -p "$RAILS_PORT"
