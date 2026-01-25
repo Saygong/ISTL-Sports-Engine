dca8da0bd8e89318c130296c6691cceeb06073e1
Simone Biondo <879899@stud.unive.it>
2026-01-25 12:02:14 +0100

    fix cors: attempt 3

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before Warden::Manager, Rack::Cors do
     # noinspection RailsParamDefResolve
     resource '*',
              headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             expose:  %w[access-token expiry token-type uid client]
   end
 end
-
