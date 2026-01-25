ad9eadd325f2d1b69d22b8e1eeb33fd4c990e56d
Simone Biondo <879899@stud.unive.it>
2026-01-25 14:48:01 +0100

    update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Prevents multiple servers from listening and causing errors
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
 # This command will create the database, load the schema, and initialize it with the seed data - if any.
 # This task operates idempotently, so it can safely be called several times, but it will only perform the necessary
 # tasks once.
 bundle exec rails db:prepare
 bundle exec rails db:migrate
+
 # Start the Rails server, binding it to all network interfaces (0.0.0.0).
 # It is then accessible from outside the container on the specified port.
 bundle exec rails s -b 0.0.0.0 -p "$RAILS_PORT"
